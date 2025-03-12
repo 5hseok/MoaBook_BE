aa42d0ea63d9bfeeaf8fa09235f609671d226e12
5hseok <ohhs1010@gmail.com>
2025-03-12 22:56:52 +0900

    :recycle: refactor(PageSave): 페이지 저장 시 페이지에 들어가는 내용들(elements)만 저장하도록 수정

--- src/main/java/com/server/moabook/page/dto/request/SavePageRequestDto.java
+++ src/main/java/com/server/moabook/page/dto/request/SavePageRequestDto.java
@@ -4,15 +4,11 @@ import com.server.moabook.page.dto.ElementDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record SavePageRequestDto(
-        @NotNull(message = "책의 아이디는 비어있을 수 없습니다.")
-        Long bookId,
-        @NotNull(message = "페이지의 아이디는 비어있을 수 없습니다.")
-        Long pageId,
-        @NotNull(message = "페이지의 번호는 비어있을 수 없습니다.")
-        Long pageNumber,
         @NotNull(message = "Element 정보는 비어있을 수 없습니다.")
         @Valid
-        ElementDto elementDto
+        List<ElementDto> elementDtos
 ) {
 }
